bd0f3b193885d8e0111c91a88b196e5cddfb2971
Beomsang Kim <choas0869@naver.com>
2021-06-16 10:16:28 +0900

    print vector info 추가

--- VectorTEST/test.h
+++ VectorTEST/test.h
@@ -9,5 +9,11 @@
 using namespace std;
 
 void print_vector_info(vector<DATA>* v) {
-	cout << "Vector address" << v->data() << endl;
+	cout << "Vector address :" << v->data() << endl;
+	cout << "Vector size    :" << v->size() << endl;
+	cout << "Vector capacity:" << v->capacity() << endl;
+	cout << "Vector data    :";
+	for ( auto i : *v ) cout << i << " ";
+	cout << endl;
+	cout << "========================" << endl;
 }
